c37f7bb0bdd26166e73b121ff87d302c56d3f5d8
Aviral3102000 <119076327+Aviral3102000@users.noreply.github.com>
2022-11-26 01:01:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
-streamlit.title('My name is Aviral Shukla')
+streamlit.title('My name is Aviral Shukla and this is my Breakfast menu')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
